8fcf21e3a16c46334cf56f76459b0d5dbc668762
Cameron Sinko <csinko@gmail.com>
2017-09-09 11:12:17 -0400

    Rename test to main, make template extension lowerase, window now stays open and displays image.  Image is now recognized (done in grayscale)

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ frame_bgr = cv2.imread('frame.jpg')
 frame_gray = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2GRAY)
 
 template = cv2.imread('template.jpg')
-w, h, _ = template.shape[::-1]
 template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+w, h = template_gray.shape[::-1]
 
-results = cv2.matchTemplate(frame_bgr, template, cv2.TM_CCOEFF_NORMED)
+results = cv2.matchTemplate(frame_gray, template_gray, cv2.TM_CCOEFF_NORMED)
 threshold = 0.8
 location = np.where(results >= threshold)
 
 for pt in zip(*location[::-1]):
     cv2.rectangle(frame_bgr, pt, (pt[0]+w, pt[1]+h), (0,255,255), 2)
 
-cv2.imshow('detected', frame_gray)
-time.sleep(1000)
+cv2.imshow('detected', frame_bgr)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
